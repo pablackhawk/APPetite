c58724a82ed2db7f349ef811d5e4670db2be49c7
pablackhawk <l.tirtarahardja@gmail.com>
2017-11-15 20:38:12 -0800

    added base html file for delivery

--- assets/javascript/delivery.js
+++ assets/javascript/delivery.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
+  var queryaddress = '11090+Santa+Monica+Blvd+Los+Angeles+CA,+90025'
 
-	var queryaddress = '11090+Santa+Monica+Blvd+Los+Angeles+CA,+90025'
+  var queryURL = 'https://api.eatstreet.com/publicapi/v1/restaurant/search-test?method=both&street-address=' +
+					queryaddress + '&access-token=e4d79c106ba7c4b2'
 
-	var queryURL = 'https://api.eatstreet.com/publicapi/v1/restaurant/search-test?method=both&street-address='
-					+ queryaddress + '&access-token=e4d79c106ba7c4b2'
-	
-	$.ajax({
-		url: queryURL,
-		method: 'GET'
-	}).done(function (response) {
-		console.log(response)
-		var results = response.data
-	})
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).done(function (response) {
+    console.log(response)
+    var results = response.data
+  })
 })
-
