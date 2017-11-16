11712e28d8d30830186c396ae89701ba2d23f680
akaryanreynolds <ryanreynolds.ca@gmail.com>
2017-11-16 10:34:38 -0800

    Update to the URL Query
    Changed how the URL query returns the new objects in the array.

--- assets/javascript/goingout.js
+++ assets/javascript/goingout.js
@@ -1,50 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-// http://opentable.herokuapp.com/api/restaurants?zip=92691
+       function getData(zipCode){
+            let url = "http://opentable.herokuapp.com/api/restaurants?zip=" + zipCode;
+            fetch(url)
+            .then((resp) => resp.json())
+            .then(function(data) {
+                console.log(data);
+                $("#test").text(JSON.stringify(data));
+            });
+       }
 
-function searchZipcode (){
-	
-	var queryZipcode = $("#zipcodeInput").val().trim();//$(this).data("zipcodeInput");
-	//console.log(queryZipcode);
-
-	var queryURL = "http://opentable.herokuapp.com/api/restaurants?zip=" + queryZipcode;
-	console.log(queryURL);
-	/*$.ajax({
-		url: queryURL,
-		method: "GET"
-	})
-	.done(function(response) {
-		console.log(queryURL);
-		console.log(response);
-	});*/
-
-	fetch(queryURL)
-	.then((resp) => resp.json())
-	.then(function(data){
-		console.log(data);
-	})
-	
-	alert("Hello");
-
-}
-
-	$("#submitZipcode").on("click", function(){
-		searchZipcode();
-	});
+       $("#submitZipcode").on("click", function(){
+           var zipCode = $("#zipcodeInput").val().trim()
+           getData(zipCode);
+       })
 
 
+     
+       
 
-
-
-
-
-
-
-
-
-
-
-
-
-});
+    });
 //Document ready closing tag
