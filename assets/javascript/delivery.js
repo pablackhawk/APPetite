762239e4067b8cec43a5c5648f0e0e758ba95293
pablackhawk <l.tirtarahardja@gmail.com>
2017-11-15 21:50:08 -0800

    started restaurant display

--- assets/javascript/delivery.js
+++ assets/javascript/delivery.js
@@ -1,14 +1,40 @@
 $(document).ready(function () {
-  var queryaddress = '11090+Santa+Monica+Blvd+Los+Angeles+CA,+90025'
+  function displayRestaurants () {
+    $('.restaurant-display').empty()
 
-  var queryURL = 'https://api.eatstreet.com/publicapi/v1/restaurant/search-test?method=both&street-address=' +
-					queryaddress + '&access-token=e4d79c106ba7c4b2'
+    var queryaddress = '54+E+Peltason+Dr,+Irvine,+CA+92617'
 
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).done(function (response) {
-    console.log(response)
-    var results = response.data
-  })
+    var queryURL = 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=delivery&street-address=' +
+			queryaddress + '&access-token=e4d79c106ba7c4b2'
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function (response) {
+      console.log(response)
+      var results = response.restaurants
+      if (results === '') {
+        console.log('There are no results for that search')
+	  }
+	  for (var i = 0; i<results.length; i++) {
+		  var restaurantDiv = $('<div class="restaurant">')
+		  var restaurantName = results[i].name
+		  var p1 = $('<p>').text(restaurantName)
+
+		  var restaurantLogo = $('<img>')
+		  restaurantDiv.attr('src', results[i].logoURL)
+		  restaurantLogo.attr('class', 'restaurant-logo')
+
+		  var restaurantAddress = results[i].streetAddress
+		  var restaurantCity = results[i].city
+		  var restaurantState = results[i].state
+		  var restaurantZip = results[i].zip
+		  var p2 = $('<p>').text(restaurantAddress + ', ' + restaurantCity + ', ' + restaurantState + ', ' + restaurantZip)
+		  restaurantDiv.append(p1)
+		  rest
+		  restaurantDiv.prepend(restaurantLogo)
+	  }
+    })
+  }
+  displayRestaurants()
 })
