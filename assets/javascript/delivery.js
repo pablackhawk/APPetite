3c67f11d269d33a0f645be3c54d7194723d24dc0
pablackhawk <l.tirtarahardja@gmail.com>
2017-11-18 11:52:33 -0800

    made form live

--- assets/javascript/delivery.js
+++ assets/javascript/delivery.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
   $('#submit-address').on('click', function (event) {
     event.preventDefault()
     $('.restaurant-display').empty()
-    // var queryAddress = $('#address-input').val().trim()
-    var queryAddress = '54+E+Peltason+Dr,+Irvine,+CA+92617'
+    var queryAddress = $('#address-input').val().trim()
+    // var queryAddress = '54+E+Peltason+Dr,+Irvine,+CA+92617'
     var queryURL = 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=delivery&street-address=' +
     queryAddress + '&access-token=e4d79c106ba7c4b2'
     $('.restaurant-display').append('Showing results for: ' + queryAddress)
