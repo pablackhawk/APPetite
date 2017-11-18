cdec001cafcd0483317be8babd6cac870aef0c98
akaryanreynolds <ryanreynolds.ca@gmail.com>
2017-11-18 13:58:04 -0800

    Adding the resturant divs
    Divs now append for the resturants after queried

--- assets/javascript/goingout.js
+++ assets/javascript/goingout.js
@@ -6,9 +6,39 @@ $(document).ready(function(){
             .then((resp) => resp.json())
             .then(function(data) {
                 console.log(data);
-                $("#mobile-url").text(JSON.stringify(data));
+                
+                for (var i = 0; i < data.restaurants.length; i++){
+
+                var restaurantsDiv = $("<div>");
+                var picContainer = $("<img>");
+                var photo = data.restaurants[i].image_url;
+                var name = data.restaurants[i].name;
+                var address = data.restaurants[i].address;
+                var city = data.restaurants[i].city;
+                var state = data.restaurants[i].state;
+                var phone = data.restaurants[i].phone;
+                var reservationLink = data.restaurants[i].mobile_reserve_url;          
+
+                console.log(restaurantsDiv);
+                picContainer.attr("src", photo);
+                // photo.attr(height:100px; width: 100px)
+                restaurantsDiv.append(picContainer);
+                restaurantsDiv.append(name);
+                restaurantsDiv.append(address);
+                restaurantsDiv.append(city);
+                restaurantsDiv.append(state);
+                restaurantsDiv.append(phone);
+                restaurantsDiv.append(reservationLink);
+
+                $("#resturant-options").prepend(restaurantsDiv);
+
+                }
             });
        }
+
+
+
+
 
        $("#submit-zipcode").on("click", function(){
            var zipCode = $("#zipcode-input").val().trim()
