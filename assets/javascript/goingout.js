5d4cbbf0d30c94d71136e75d6a1b7fad10580b3e
pablackhawk <l.tirtarahardja@gmail.com>
2017-11-17 22:18:49 -0800

    Added code for displaying restaurant results, need to fix entry via enter

--- assets/javascript/goingout.js
+++ assets/javascript/goingout.js
@@ -1,23 +1,17 @@
-$(document).ready(function(){
-
-       function getData(zipCode){
-            let url = "http://opentable.herokuapp.com/api/restaurants?zip=" + zipCode;
-            fetch(url)
+$(document).ready(function () {
+  function getData (zipCode) {
+    let url = 'http://opentable.herokuapp.com/api/restaurants?zip=' + zipCode
+    fetch(url)
             .then((resp) => resp.json())
-            .then(function(data) {
-                console.log(data);
-                $("#test").text(JSON.stringify(data));
-            });
-       }
+            .then(function (data) {
+              console.log(data)
+              $('#test').text(JSON.stringify(data))
+            })
+  }
 
-       $("#submitZipcode").on("click", function(){
-           var zipCode = $("#zipcodeInput").val().trim()
-           getData(zipCode);
-       })
-
-
-     
-       
-
-    });
-//Document ready closing tag
+  $('#submitZipcode').on('click', function () {
+    var zipCode = $('#zipcodeInput').val().trim()
+    getData(zipCode)
+  })
+})
+// Document ready closing tag
