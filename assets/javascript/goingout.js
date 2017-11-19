c8641e36f6f87ddb524e0ff69eb2b931141243c8
akaryanreynolds <ryanreynolds.ca@gmail.com>
2017-11-18 17:27:25 -0800

    Info to Paragrahs
    Changed info to <p>

--- assets/javascript/goingout.js
+++ assets/javascript/goingout.js
@@ -17,18 +17,19 @@ $(document).ready(function(){
                 var city = data.restaurants[i].city;
                 var state = data.restaurants[i].state;
                 var phone = data.restaurants[i].phone;
-                var reservationLink = data.restaurants[i].mobile_reserve_url;          
+                var reservationLink = data.restaurants[i].mobile_reserve_url; 
+                var p = $("<p>")          
 
                 console.log(restaurantsDiv);
                 picContainer.attr("src", photo);
                 // photo.attr(height:100px; width: 100px)
                 restaurantsDiv.append(picContainer);
-                restaurantsDiv.append(name);
-                restaurantsDiv.append(address);
-                restaurantsDiv.append(city);
-                restaurantsDiv.append(state);
-                restaurantsDiv.append(phone);
-                restaurantsDiv.append(reservationLink);
+                restaurantsDiv.append(p);
+                p.append(address);
+                p.append(city);
+                p.append(state);
+                p.append(phone);
+                p.append(reservationLink);
 
                 $("#resturant-options").prepend(restaurantsDiv);
 
