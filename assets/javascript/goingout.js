016fb9b5758497e9ad23359a7b65d2bfd29c8d68
Brendan Kim <brendankim@dhcp-v102-022.mobile.uci.edu>
2017-11-27 19:17:24 -0800

    Last Minute Changes
    Last minute changes

--- assets/javascript/goingout.js
+++ assets/javascript/goingout.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
               for (var i = 0; i < data.restaurants.length; i++) {
                 var restaurantsDiv = $('<div data-aos="flip-left" class="small-12 columns restaurant">')
-                var picContainer = $('<img class="restaurant-logo">')
+                var picContainer = $('<img class="restaurant-logo-dineout">')
                 var photo = data.restaurants[i].image_url
                 var name = data.restaurants[i].name
                 var address = data.restaurants[i].address
